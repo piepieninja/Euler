b4ae8f4787cd1bf5e23fca1bd1ed81a728f3b59d
Caleb Adams <CalebAshmoreAdams@gmail.com>
2015-10-28 01:14:56 -0400

    adds java

---
+++ problems/001/answer.java
@@ -0,0 +1,12 @@
+public class answer {
+    public static void main(String[] args) {
+	int ret = 0;
+	for (int i = 0; i < 1000; i++){
+	    if (i % 3 == 0 || i % 5 == 0){
+		ret += i;
+	    }
+	}
+
+	System.out.println("answer: " + ret);	
+    }
+}
