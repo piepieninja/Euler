ebd238bcf7143279a726a80d91440787c381b273
Caleb Adams <CalebAshmoreAdams@gmail.com>
2015-11-01 02:23:41 -0500

    adds bash to problem 2

--- problems/002/answer.sh
+++ problems/002/answer.sh
@@ -2,9 +2,8 @@
 ##
 # Author: Caleb Adams
 # Discription: This is a very simple algorithm that runs in
-# linear time O(n) to compute the sum of all the multiples
-# of 3 or 5 below int i.
-## 
+# linear time O(n) to compute the sum of all even fib numebrs
+##
 
 ret=0
 fib_a=0
