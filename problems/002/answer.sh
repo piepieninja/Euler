646b2976359bc522ca322265a779413647d15989
Caleb Adams <CalebAshmoreAdams@gmail.com>
2015-11-01 02:22:39 -0500

    adds bash to problem 2

---
+++ problems/002/answer.sh
@@ -0,0 +1,26 @@
+
+##
+# Author: Caleb Adams
+# Discription: This is a very simple algorithm that runs in
+# linear time O(n) to compute the sum of all the multiples
+# of 3 or 5 below int i.
+## 
+
+ret=0
+fib_a=0
+fib_b=1
+fib_c=1
+max=4000000
+while [ $fib_c -lt $max ]
+do
+      fib_c=$((fib_a + fib_b))
+      fib_a=$fib_b
+      fib_b=$fib_c
+      if [ $((fib_c % 2)) -eq 0 ]
+      then
+	 ret=$((ret + fib_c))
+      fi
+done
+
+
+echo "answer: $ret"
