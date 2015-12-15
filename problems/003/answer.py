c8a97678f38e82c58892ecd09931bc0f7857d02d
Caleb Adams <CalebAshmoreAdams@gmail.com>
2015-12-15 11:14:32 -0500

    python answer 3

---
+++ problems/003/answer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+##
+# Author: Caleb Adams
+# Discription: This is a very simple algorithm that runs in
+# linear time O(n) to list the prime factors of a number.
+##
+    
+
