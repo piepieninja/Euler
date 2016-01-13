d312152feba428924afb63f937fc3daca4ed3563
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-12 20:23:46 -0500

    adds stubs for 000

--- problems/003/answer.sh
+++ problems/003/answer.sh
@@ -5,3 +5,16 @@
 # Discription: This is a very simple algorithm that runs in
 # linear time O(n) to list the prime factors of a number.
 ##
+
+target=600851475143
+i=3
+
+while [ $((target % 2)) -eq 0 ]
+do
+  echo "2 "
+  target=$((target/2))
+done
+
+while [[ $i - ]]; do
+  #statements
+done
