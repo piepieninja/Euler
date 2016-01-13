b9d0c0cda41605d126c0fc53cc7ae8521aec0a7c
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-12 19:33:48 -0500

    stubs for problem 3

--- problems/003/answer.rb
+++ problems/003/answer.rb
@@ -1,13 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env ruby
 
 ##
 # Author: Caleb Adams
 # Discription: This is a very simple algorithm that runs in
 # linear time O(n) to list the prime factors of a number.
 ##
-
-largenum = 600851475143
-
-while (largenum % 2 == 0):
-    print 2
-    largenum = largenum / 2;
