c73fc1206715efa3fa895b94841e10846934c5c0
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-12 20:53:15 -0500

    adds stubs for problem 4

---
+++ problems/004/answer.java
@@ -0,0 +1,14 @@
+//package 001;
+
+/**
+ * Author: Caleb Adams
+ * Discription: This is a very simple algorithm that runs in
+ * linear time O(n) to compute the sum of all even fib numbers
+ */
+
+
+public class answer {
+    public static void main(String[] args) {
+
+    }
+}
