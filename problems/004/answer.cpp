a69264f002c7bfab62e41a802722d2428d891d6d
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-12 21:21:25 -0500

    solves 004 with c++

--- problems/004/answer.cpp
+++ problems/004/answer.cpp
@@ -4,5 +4,33 @@ using namespace std;
 
 /**
  * Author: Caleb Adams
- * Discription:
+ * Discription: This a brute force approach to finding the largest pallendrome of two 3-digit multiples
+ * This algorithm operates in O(n^2) time
+ * Method: isPallandrome. returns 1 if so and 0 if not!
  */
+
+
+bool isPallandrome(int x){
+  int reverse = 0;
+  int n = x;
+  while (n != 0){
+    reverse = reverse * 10;
+    reverse = reverse + n%10;
+    n       = n/10;
+  }
+  return (x == reverse);
+}
+
+int main(){
+  int max_pall = 0;
+  for (int i = 999; i > 100; i--){
+    for (int j = 999; j > 100; j--){
+      if (isPallandrome(i*j) && (i*j > max_pall) ) {
+        max_pall = i * j;
+        cout << i << " * " << j << " = " << max_pall << "\n";
+        // will eventually find the largest
+      }
+    }
+  }
+  return 0;
+}
