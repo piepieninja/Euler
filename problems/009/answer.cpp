762b137159fc1c5d74dc350ea29884e653bb067d
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-13 22:12:52 -0500

    solves problem 009

--- problems/009/answer.cpp
+++ problems/009/answer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -7,6 +8,21 @@ using namespace std;
  * Discription:
  */
 
+int square(int x) {
+  return x*x;
+}
+
 int main(){
+
+  int a = 1;
+  int b = 1;
+  for (a = 1; a < 999; a++){
+    for (b = 1; b < 999; b++){
+      if ((a + b + sqrt(square(a) + square(b))) == 1000)
+      cout << "answer: " << (a * b * sqrt(square(a) + square(b))) << endl;
+    }
+  }
+
+
   return 0;
 }
