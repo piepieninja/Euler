b7b902b0d266600f0cbe7665f4d33b75fed59708
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-14 11:58:27 -0500

    adds stubs for problem 011

---
+++ problems/011/answer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Author: Caleb Adams
+ * Discription: This is a simple "rotating" sliding window algorithm.  
+ */
+
+
+
+int main(){
+
+  return 0;
+}
