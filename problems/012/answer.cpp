317d4a21c0a6b3433f694525b307af27447db271
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-18 07:26:57 -0500

    solves 004 in python

--- problems/012/answer.cpp
+++ problems/012/answer.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <climits>
+#include <cmath>
 
 using namespace std;
 
@@ -7,7 +9,27 @@ using namespace std;
  * Discription:
  */
 
+int root(int x){
+  return (int) sqrt(x) + 1;
+}
+
+int enumerateFactors(int x){
+  int result = 2;
+
+  return result;
+}
+
 int main(){
+
+  int sum = 0;
+  int factorNum;
+  int target = 500;
+  for (int i = 0; i < INT_MAX; i++){
+    sum += i;
+    int factorNum = enumerateFactors(sum);
+    if (factorNum >= target) break;
+    cout << i << ": " << sum << " => " << factorNum << endl;
+  }
 
   return 0;
 }
