dfb36eb5d3d6f989ab3fd27a3dcec318b7ec76e2
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-18 20:55:17 -0700

    problem 012 updates

--- problems/012/answer.cpp
+++ problems/012/answer.cpp
@@ -9,27 +9,41 @@ using namespace std;
  * Discription:
  */
 
+int lookUpTable[INT_MAX] = {0}; 
+
 int root(int x){
   return (int) sqrt(x) + 1;
 }
 
 int enumerateFactors(int x){
   int result = 2;
-
+  for (int i = 1; i < (x/2); i++){
+    if (!(x%i)) result++;
+  }
   return result;
+}
+
+int factorLookUp(int x){
+  if (!lookUpTable[x]) lookUpTable[x] = enumerateFactors(x);
+  return lookUpTable[x];
 }
 
 int main(){
 
+  // just for testing
+  int currentMax = 0;
+  
   int sum = 0;
   int factorNum;
   int target = 500;
   for (int i = 0; i < INT_MAX; i++){
     sum += i;
-    int factorNum = enumerateFactors(sum);
+    int factorNum = factorLookUp[sum];
     if (factorNum >= target) break;
-    cout << i << ": " << sum << " => " << factorNum << endl;
+    if (factorNum >= currentMax){
+      currentMax = factorNum;
+      cout << i << ": " << sum << " => " << factorNum << endl;     
+    }
   }
-
   return 0;
 }
