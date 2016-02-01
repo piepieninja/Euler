f6a7ce206eca2b5c46706532d99026b15ddcbb5a
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-31 20:49:08 -0500

    builds subtraction

--- lib/LargeNumber.cpp
+++ lib/LargeNumber.cpp
@@ -23,13 +23,21 @@ private:
 
   void setValueAtIndex(char, int);
 
+  void decrement();
+
+  void subtract(int);
+
   void add(int);
   void add(int, int);
   void add(LargeNumber);
 
   void multiply(int);
+  void multiply(LargeNumber);
 
-  void pow(int);
+  void power(int);
+
+  bool equals(int);
+  bool equals(LargeNumber);
 
   char valueAtIndex(int);
 
@@ -76,6 +84,22 @@ void LargeNumber::setNumber(LargeNumber l){
 
 void LargeNumber::setValueAtIndex(char ch, int x){
   array[x] = ch;
+}
+
+void LargeNumber::subtract(int x){ // adds int x to the array at possition fromIndex
+  for (int i = 0; x > 0; i++){
+    if (array[i] - x%10 >= 0) {
+      array[i] -= x%10;
+      x /= 10;
+    } else { // oh boy here we go
+      array[i] = 9;
+      array[i+1]--;
+      if (array[i+1] == 0){
+        digits--;
+      }
+      x /= 10;
+    }
+  }
 }
 
 void LargeNumber::add(int x){ // adds int x to the array at possition fromIndex
@@ -136,15 +160,47 @@ void LargeNumber::add(LargeNumber l){
 void LargeNumber::multiply(int x){
     LargeNumber l;
     l.setNumber(* this);
-    x--;
+    x--; // don't multiply by zero!
     while (x){
       this->add(l);
       x--;
     }
 }
 
-void LargeNumber::pow(int x){
+void LargeNumber::multiply(LargeNumber l){
+    LargeNumber temp;
+    temp.setNumber(* this);
+    l.subtract(1); // don't multiply by zero!
+    while (!(l.getDigits() == 1 && l.equals(0))){ // the digits speeds it up
+      this->add(temp);
+      l.subtract(1);
+    }
+}
 
+void LargeNumber::power(int x){
+  LargeNumber l;
+  l.setNumber(* this);
+  x--; // don't multiply by zero!
+  while (x){
+    this->multiply(l);
+    x--;
+  }
+}
+
+// ====== BOOL RETURNS HERE ======
+bool LargeNumber::equals(int x){
+  LargeNumber l;
+  l.setNumber(x);
+  if (digits != l.getDigits()) return false; // quick check
+  for (int i = 0; i < digits; i++){ // long check
+    if (this->valueAtIndex(i) == l.valueAtIndex(i)) return false;
+  }
+  return true;
+}
+
+bool LargeNumber::equals(LargeNumber l){
+  //TODO implement
+  return false;
 }
 
 // ====== char RETURNS HERE ======
