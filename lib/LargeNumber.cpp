94b3f06b1918ca28c2d67fc02bcfc1700326816c
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-31 19:09:53 -0500

    class can add itself

--- lib/LargeNumber.cpp
+++ lib/LargeNumber.cpp
@@ -12,6 +12,7 @@
 using namespace std;
 
 class LargeNumber{
+private:
   char   array[100000];
   int    digits;
  public:
@@ -19,8 +20,13 @@ class LargeNumber{
   void setNumber(long);
   void setNumber(string);
 
+  void setValueAtIndex(char, int);
+
   void add(int);
-  void add(string);
+  void add(int, int);
+  void add(LargeNumber);
+
+  char valueAtIndex(int);
 
   int getDigits();
 
@@ -52,9 +58,13 @@ void LargeNumber::setNumber(long x){
 
 void LargeNumber::setNumber(string str){
   digits = str.length();
-  for (int i = 0; i < digits; i++){
-    array[i] = str[i] - 48; // assumes ASCII
+  for (int i = 0; i < digits ; i++){
+    array[digits - 1 - i] = str[i] - 48; // assumes ASCII
   }
+}
+
+void LargeNumber::setValueAtIndex(char ch, int x){
+  array[x] = ch;
 }
 
 void LargeNumber::add(int x){ // adds int x to the array at possition fromIndex
@@ -74,11 +84,48 @@ void LargeNumber::add(int x){ // adds int x to the array at possition fromIndex
     x += carry;
     }
   }
-
 }
 
-void LargeNumber::add(string str){
-  // TODO implement this
+void LargeNumber::add(int x, int fromIndex){ // adds int x to the array at possition fromIndex
+  for (int i = fromIndex; x > 0; i++){
+    if (array[i] + x%10 < 10) {
+      array[i] += x%10;
+      x /= 10;
+    } else { // oh boy here we go
+    if (i == digits - 1) {
+      digits++; // increase digit count and clear previous data
+      array[i+1] = 0;
+    }
+    int val        = (array[i] + x%10 - 10);
+    int carry      = ((array[i] + x%10) / 10);
+    array[i] = val;
+    x /= 10;
+    x += carry;
+    }
+  }
+}
+
+void LargeNumber::add(LargeNumber l){
+  for (int i = 0; i < digits; i++){
+    if (this->valueAtIndex(i) + l.valueAtIndex(i) < 10){
+      this->setValueAtIndex(this->valueAtIndex(i) + l.valueAtIndex(i), i);
+    } else {
+      if (i == digits - 1){
+      	digits++;
+      	this->setValueAtIndex(0, i + 1);
+      }
+      int val   = (this->valueAtIndex(i) + l.valueAtIndex(i)%10 - 10);
+      int carry = ((this->valueAtIndex(i) + l.valueAtIndex(i)%10) / 10);
+      this->setValueAtIndex(val, i);
+      this->add(carry, i+1); // add the left overs!
+    }
+  }
+}
+
+// ====== char RETURNS HERE ======
+
+char LargeNumber::valueAtIndex(int x){
+  return array[x];
 }
 
 // ====== INT RETURNS HERE ======
